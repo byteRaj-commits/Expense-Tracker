42ab3dcb9b79753a9bd7671fb666ceb3a0839669
byteRaj-commits <rajp182138@gmail.com>
2026-02-02 09:40:04 +0530

    fetching data from html with its id

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 let transaction = [
     {
         id: 1,
-        date: "2026-01-26",
-        amount: -440,
+        date: "2026-01-14",
+        amount: -299.05,
+        status: "success",
+        type: "expense",
+    },
+
+    {
+        id: 2,
+        date: "2026-01-8",
+        amount: -583,
+        status: "success",
+        type: "expense",
+    },
+
+    {
+        id: 3,
+        date: "2026-01-2",
+        amount: -8500,
         status: "success",
         type: "expense",
     },
 ]
+
+let monthlyIncome = 5252;
+let monthlyExpenses = 5698;
+
+const today = new Date().toISOString().split('T')[0]
+document.getElementById('incomeDate').value = today
+document.getElementById('expenseDate').value = today
+
+function openIncomeModal(){
+    document.getElementById('incomeModal').style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+

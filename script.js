312b7979a707c09144f6d343dca6d26e5797b6d9
byteRaj-commits <rajp182138@gmail.com>
2026-02-03 10:53:44 +0530

    updating transaction table data

--- script.js
+++ script.js
@@ -130,3 +130,48 @@ function addExpense(){
     showNotification("Expense added successfully", "success");
 
 }
+
+function updateDashboard(){
+    document.querySelector('.income-amount').textContent = `${monthlyIncome.toLocaleString()}.00`
+    document.querySelector('.expense-amount').textContent = `${monthlyExpense.toLocaleString()}.00`
+
+    let spendingLimit = 12345;
+    const usedAmount = monthlyExpenses;
+    const percentage = (usedAmount / spendingLimit) * 100;
+
+    document.querySelector('.spending-limit').textContent = `${(spendingLimit - usedAmount).toLocaleString()}.00`
+    document.querySelector('.progess-fill').style.width = `${Math.min(percentage, 100)}%`
+
+}
+
+function updateTransactionTable() {
+    const tbody = document.querySelector('.transactions-table tbody');
+    tbody.innerHTML = '';
+
+    const recentTransactions = transaction.slice(0, 10);
+
+    recentTransactions.forEach((transaction) => {
+        const row = document.createElement('tr');
+        const formattedDate = new Date(transaction.date).toLocaleDateString(
+            'en-us',
+            {
+                months: 'short',
+                day: 'numeric',
+                year: 'numeric',
+            }
+        );
+
+        const amountDisplay = transaction.amount > 0 ? `+${transaction.amount.toLocaleString()}.00` : `-${Math.abs(transaction.amount).toLocaleString()}.00`
+
+        row.innerHTML = `
+            <td>${formattedDate}</td>
+            <td>${transaction.category}</td>
+            <td style="color: ${transaction.amount > 0 ? "#10b981" : "#ef4444"}" >${amountDisplay}</td>
+            <td><span class="status-success">${transaction.status}</span></td>
+            <td><button class="action-btn"><i class="fas fa-ellipsis-h"></i></button></td>
+        `;
+
+        tbody.appendChild(row)
+
+    });
+}
